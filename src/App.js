9c90f5ed70842b0110061c0f9c3f563da2437773
eleazar <eleazarfhernandez@gmail.com>
2023-07-27 17:00:57 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -1,66 +1,5 @@
-/*
-const Header = (header) => {
-  console.log(header)
-  return(<h1>{header.name}</h1>)
-}
-
-const Part = (content) => {
-  return (<div> <p>{content.name} {content.exercise}</p> </div>)
-}
-
-const Content = (content) => {
-  return (
-      <div>
-        <Part name={content.parts[0].names}
-        exercise={content.parts[0].exercise}
-        />
-        <Part name={content.parts[1].names}
-              exercise={content.parts[1].exercise}
-        />
-        <Part name={content.parts[2].names}
-              exercise={content.parts[2].exercise}
-        />
-      </div>
-  )
-}
-
-const Total = (total) => {
-  return (
-      <div>
-        <p>Total:{" "}{total.parts[0].exercise + total.parts[1].exercise + total.parts[2].exercise}</p>
-      </div>
-  )
-}
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        names: 'Fundamentals of React',
-        exercise: 10,
-      },
-      {
-        names: 'Using props to pass data',
-        exercise: 7,
-      },
-      {
-        names: 'State of a component',
-        exercise: 14,
-      },
-    ],
-  };
-
-  return(
-      <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-        </div>
-        )
-}*/
-
 import { useState} from "react";
+import "./styles.css";
 
 const Display = ({ counter }) => <div>{counter}</div>
 
@@ -74,17 +13,15 @@ const App = () =>{
     const zeroReset = () => setCounter(0)
     const negativeClick = () => setCounter(counter - 1)
 
-
-    console.log("rendering, " + counter)
     return (
         <div>
         <Display counter={counter}/>
         <Button handleClick={oneClick}
-        text="Plus" />
+        text="Plus" onClick={handleSubmit} />
         <Button handleClick={negativeClick}
         text="Minus" />
         <Button handleClick={zeroReset}
-        text="Zero babayyy" />
+        text="Zero" />
         </div>
     )
 }
